06a75085a1e4179d5f404546f0fe6cb3ded1c3a0
Sallta <139439807+Sallta@users.noreply.github.com>
2023-07-23 23:29:00 +0300

    Update Program.cs

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -4,7 +4,7 @@
 // 8 -> 2, 4, 6, 8
 
 Console.WriteLine("Введите число больше 1");
-int number = Int32.Parse(Console.ReadLine()!);
+int number = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Чётные числа от 1 до " + number + ":");
 for (int i = 2; i <= number; i += 2)
